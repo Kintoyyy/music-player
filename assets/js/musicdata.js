b9c303c34866e072f645bd1b8e442c1d08ca29c8
kintoyyy <56209108+Kintoyyy@users.noreply.github.com>
2023-06-04 20:31:37 +0800

    new music

---
+++ assets/js/musicdata.js
@@ -0,0 +1,539 @@
+/**
+ * all music information
+ */
+
+
+
+const musicData = [
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Area Codes",
+        "album": "",
+        "year": "",
+        "artist": "Kali",
+        "musicPath": "./assets/music/Area Codes.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "10,000 Hours (with Justin Bieber)",
+        "album": "",
+        "year": "",
+        "artist": "Dan + Shay/Justin Bieber",
+        "musicPath": "./assets/music/10,000 Hours (with Justin Bieber).mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "8 Letters",
+        "album": "",
+        "year": "",
+        "artist": "Why Don't We",
+        "musicPath": "./assets/music/8 Letters.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "All I Ever Need",
+        "album": "",
+        "year": "",
+        "artist": "Austin Mahone",
+        "musicPath": "./assets/music/All I Ever Need.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "All I Want - From \"High School Musical: The Musical: The Series\"",
+        "album": "",
+        "year": "",
+        "artist": "Olivia Rodrigo/Disney",
+        "musicPath": "./assets/music/All I Want - From _High School Musical_ The Musical_ The Series_.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "BRB",
+        "album": "",
+        "year": "",
+        "artist": "Luh Kel",
+        "musicPath": "./assets/music/BRB.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Be Alright",
+        "album": "",
+        "year": "",
+        "artist": "Dean Lewis",
+        "musicPath": "./assets/music/Be Alright.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Before You Go",
+        "album": "",
+        "year": "",
+        "artist": "Lewis Capaldi",
+        "musicPath": "./assets/music/Before You Go.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Boy's a Liar Pt. 2",
+        "album": "",
+        "year": "",
+        "artist": "PinkPantheress/Ice Spice",
+        "musicPath": "./assets/music/Boy's a Liar Pt. 2.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "California King Bed",
+        "album": "",
+        "year": "",
+        "artist": "Rihanna",
+        "musicPath": "./assets/music/California King Bed.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Can I Be Him",
+        "album": "",
+        "year": "",
+        "artist": "James Arthur",
+        "musicPath": "./assets/music/Can I Be Him.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Count Me In",
+        "album": "",
+        "year": "",
+        "artist": "Cast - Liv and Maddie",
+        "musicPath": "./assets/music/Count Me In.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Dancing With Your Ghost",
+        "album": "",
+        "year": "",
+        "artist": "Sasha Alex Sloan",
+        "musicPath": "./assets/music/Dancing With Your Ghost.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Dandelions",
+        "album": "",
+        "year": "",
+        "artist": "Ruth B.",
+        "musicPath": "./assets/music/Dandelions.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Eastside (with Halsey & Khalid)",
+        "album": "",
+        "year": "",
+        "artist": "benny blanco/Halsey/Khalid",
+        "musicPath": "./assets/music/Eastside (with Halsey & Khalid).mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Falling",
+        "album": "",
+        "year": "",
+        "artist": "Harry Styles",
+        "musicPath": "./assets/music/Falling.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Fly",
+        "album": "",
+        "year": "",
+        "artist": "Nicki Minaj/Rihanna",
+        "musicPath": "./assets/music/Fly.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Gold",
+        "album": "",
+        "year": "",
+        "artist": "Kiiara",
+        "musicPath": "./assets/music/Gold.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "I Don’t Wanna Live Forever (Fifty Shades Darker)",
+        "album": "",
+        "year": "",
+        "artist": "ZAYN/Taylor Swift",
+        "musicPath": "./assets/music/I Don’t Wanna Live Forever (Fifty Shades Darker).mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "I Quit Drinking",
+        "album": "",
+        "year": "",
+        "artist": "Kelsea Ballerini/LANY",
+        "musicPath": "./assets/music/I Quit Drinking.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "It's You",
+        "album": "",
+        "year": "",
+        "artist": "Ali Gatie",
+        "musicPath": "./assets/music/It's You.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Kill Bill",
+        "album": "",
+        "year": "",
+        "artist": "SZA",
+        "musicPath": "./assets/music/Kill Bill.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Lose You To Love Me",
+        "album": "",
+        "year": "",
+        "artist": "Selena Gomez",
+        "musicPath": "./assets/music/Lose You To Love Me.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Malibu Nights",
+        "album": "",
+        "year": "",
+        "artist": "LANY",
+        "musicPath": "./assets/music/Malibu Nights.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "My Only One (No Hay Nadie Más)",
+        "album": "",
+        "year": "",
+        "artist": "Sebastian Yatra/Isabela Merced",
+        "musicPath": "./assets/music/My Only One (No Hay Nadie Más).mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "No One Compares To You",
+        "album": "",
+        "year": "",
+        "artist": "Jack & Jack",
+        "musicPath": "./assets/music/No One Compares To You.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Nobody Compares To You (feat. Katie Pearlman)",
+        "album": "",
+        "year": "",
+        "artist": "Gryffin/Katie Pearlman",
+        "musicPath": "./assets/music/Nobody Compares To You (feat. Katie Pearlman).mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "On My Way",
+        "album": "",
+        "year": "",
+        "artist": "Illijah",
+        "musicPath": "./assets/music/On My Way.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Once in a Lifetime",
+        "album": "",
+        "year": "",
+        "artist": "Freestyle",
+        "musicPath": "./assets/music/Once in a Lifetime.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Open Arms (feat. Travis Scott)",
+        "album": "",
+        "year": "",
+        "artist": "SZA/Travis Scott",
+        "musicPath": "./assets/music/Open Arms (feat. Travis Scott).mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Pull Up",
+        "album": "",
+        "year": "",
+        "artist": "Luh Kel",
+        "musicPath": "./assets/music/Pull Up.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Remember - Acoustic",
+        "album": "",
+        "year": "",
+        "artist": "Becky Hill",
+        "musicPath": "./assets/music/Remember - Acoustic.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Rewrite The Stars (with James Arthur & Anne-Marie)",
+        "album": "",
+        "year": "",
+        "artist": "James Arthur/Anne-Marie",
+        "musicPath": "./assets/music/Rewrite The Stars (with James Arthur & Anne-Marie).mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Ring",
+        "album": "",
+        "year": "",
+        "artist": "Selena Gomez",
+        "musicPath": "./assets/music/Ring.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Ruin My Life",
+        "album": "",
+        "year": "",
+        "artist": "Zara Larsson",
+        "musicPath": "./assets/music/Ruin My Life.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Scars To Your Beautiful",
+        "album": "",
+        "year": "",
+        "artist": "Alessia Cara",
+        "musicPath": "./assets/music/Scars To Your Beautiful.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Secret Love Song (feat. Jason Derulo)",
+        "album": "",
+        "year": "",
+        "artist": "Little Mix/Jason Derulo",
+        "musicPath": "./assets/music/Secret Love Song (feat. Jason Derulo).mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Shower",
+        "album": "",
+        "year": "",
+        "artist": "Becky G",
+        "musicPath": "./assets/music/Shower.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Something New",
+        "album": "",
+        "year": "",
+        "artist": "Zendaya/Chris Brown",
+        "musicPath": "./assets/music/Something New.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Strawberries & Cigarettes",
+        "album": "",
+        "year": "",
+        "artist": "Troye Sivan",
+        "musicPath": "./assets/music/Strawberries & Cigarettes.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Stuck with U (with Justin Bieber)",
+        "album": "",
+        "year": "",
+        "artist": "Ariana Grande/Justin Bieber",
+        "musicPath": "./assets/music/Stuck with U (with Justin Bieber).mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Superficial Love",
+        "album": "",
+        "year": "",
+        "artist": "Ruth B.",
+        "musicPath": "./assets/music/Superficial Love.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Take A Bow",
+        "album": "",
+        "year": "",
+        "artist": "Rihanna",
+        "musicPath": "./assets/music/Take A Bow.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Teenage Mona Lisa",
+        "album": "",
+        "year": "",
+        "artist": "Alfie Castley",
+        "musicPath": "./assets/music/Teenage Mona Lisa.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "That Should Be Me",
+        "album": "",
+        "year": "",
+        "artist": "Justin Bieber",
+        "musicPath": "./assets/music/That Should Be Me.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "The Only Exception",
+        "album": "",
+        "year": "",
+        "artist": "Paramore",
+        "musicPath": "./assets/music/The Only Exception.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "They Just Exist",
+        "album": "",
+        "year": "",
+        "artist": "Gatton/Kennedy Brown",
+        "musicPath": "./assets/music/They Just Exist.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "This City",
+        "album": "",
+        "year": "",
+        "artist": "Sam Fischer",
+        "musicPath": "./assets/music/This City.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Too Good At Goodbyes",
+        "album": "",
+        "year": "",
+        "artist": "Sam Smith",
+        "musicPath": "./assets/music/Too Good At Goodbyes.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Under The Influence",
+        "album": "",
+        "year": "",
+        "artist": "Chris Brown",
+        "musicPath": "./assets/music/Under The Influence.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "When Scars Become Art",
+        "album": "",
+        "year": "",
+        "artist": "Gatton/Brooke Young",
+        "musicPath": "./assets/music/When Scars Become Art.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Without Me",
+        "album": "",
+        "year": "",
+        "artist": "Halsey",
+        "musicPath": "./assets/music/Without Me.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "Y.O.U.",
+        "album": "",
+        "year": "",
+        "artist": "Luh Kel",
+        "musicPath": "./assets/music/Y.O.U..mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "YOUTH",
+        "album": "",
+        "year": "",
+        "artist": "Troye Sivan",
+        "musicPath": "./assets/music/YOUTH.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "You and I Collide",
+        "album": "",
+        "year": "",
+        "artist": "The Time Keepers",
+        "musicPath": "./assets/music/You and I Collide.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "deja vu",
+        "album": "",
+        "year": "",
+        "artist": "Olivia Rodrigo",
+        "musicPath": "./assets/music/deja vu.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "drivers license",
+        "album": "",
+        "year": "",
+        "artist": "Olivia Rodrigo",
+        "musicPath": "./assets/music/drivers license.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "hate u love u",
+        "album": "",
+        "year": "",
+        "artist": "Olivia O'Brien",
+        "musicPath": "./assets/music/hate u love u.mp3"
+    },
+    {
+        "backgroundImage": "./assets/images/poster-2.jpg",
+        "posterUrl": "./assets/images/poster-2.jpg",
+        "title": "traitor",
+        "album": "",
+        "year": "",
+        "artist": "Olivia Rodrigo",
+        "musicPath": "./assets/music/traitor.mp3"
+    }
+];
